f21c71e9ab15ea2fe75f4b82aaf5f0e3a213b307
sunho45 <100903674+sunho45@users.noreply.github.com>
2022-05-03 22:16:02 +0900

    Add files via upload

---
+++ DEDEE.java
@@ -0,0 +1,45 @@
+
+public class DEDEE {
+    private static void quickSort(int[] arr,int start, int end) {
+        int part=partition(arr,start,end);
+        if(start<part-1) quickSort(arr,start,part-1);
+        if(end>part) quickSort(arr,part,end);
+    }
+
+    private static int partition(int[] arr,int start,int end) {
+        int pivot=arr[(start+end)/2];
+        while(start<=end) {
+            while(arr[start]<pivot) start++;
+            while(arr[end]>pivot) end--;
+            if(start<=end) {
+                swap(arr,start,end);
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
+
+    private static void swap(int[] arr,int start,int end) {
+        int tmp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=tmp;
+        return;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[8192];
+		for(int i=0;i<arr.length;i++) {
+			
+			arr[i]=i;
+			
+		}
+        double beforeTime = System.currentTimeMillis();
+        quickSort(arr,0,arr.length-1);
+        double aftertime = System.currentTimeMillis();
+		double secDiffTime = (aftertime - beforeTime)/1000;
+		System.out.println("수행시간(s) : "+secDiffTime);
+ 
+        }
+    }
